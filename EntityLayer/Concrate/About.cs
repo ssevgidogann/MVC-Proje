c2a0d98f47dc8f2521aa4aad4b3963d393edb86a
Sevgi Doğan <101864117+ssevgidogann@users.noreply.github.com>
2022-05-31 16:57:10 +0300

    Add files via upload

---
+++ EntityLayer/Concrate/About.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+   public class About
+
+    {
+        [Key]
+
+        public int AboutID { get; set; }
+
+
+
+
+        [StringLength(1000)]  
+       public string AboutDetails { get; set; }
+       
+        [StringLength(1000)]
+        public string AboutDetails2 { get; set; }
+
+        
+        [StringLength(1000)] 
+        public string AboutImage { get; set; }
+
+        [StringLength(1000)]
+        public string AboutImage2 { get; set; }
+
+    }
+}
