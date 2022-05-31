2896fe0a7c02b04326e330de816c1e963c2c5982
Sevgi Doğan <101864117+ssevgidogann@users.noreply.github.com>
2022-05-31 16:51:50 +0300

    Add files via upload

---
+++ DataAccessLayer/Migrations/202205131109433_sütünkaldir.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sütünkaldir : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Contacts", "NameSurname");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Contacts", "NameSurname", c => c.String(maxLength: 1000));
+        }
+    }
+}
